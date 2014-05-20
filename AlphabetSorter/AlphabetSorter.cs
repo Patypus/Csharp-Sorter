0568b148a73afa23f75f74752c3577d4187f8c98
Pat Howell <patrick.howell@outlook.com>
2014-05-20 21:57:39 +0100

    Initial commit to remote

---
+++ AlphabetSorter/AlphabetSorter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSorter
+{
+    /// <summary>
+    /// A small utility to sort the contents of a file alphabtically, with the sorted conent
+    /// being written to a second file with the same name as the file being sorted appended 
+    /// with "Sorted". This class is the entry point to the program.
+    /// </summary>
+    public class AlphabetSorter
+    {
+        /// <summary>
+        /// Main entry to the program. Checks the contents of the supplied command line
+        /// arguments to ensure that there is a single path to an file existing file
+        /// </summary>
+        /// <param name="args">string array supplied by the command line</param>
+        public static void Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                PrintUsageMessage();
+            }
+            else
+            {
+                if (File.Exists(args[0]))
+                {
+                    SortFilesContents(args[0]);
+                }
+                else
+                {
+                    PrintUsageMessage();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Print message to command line explaining program usuage 
+        /// </summary>
+        private static void PrintUsageMessage()
+        {
+            Console.WriteLine("Please supply a single parameter: a path to a file whose contents you wish to sort alphaetically");
+        }
+
+        /// <summary>
+        /// Sorts the contents of the file at the path supplied by the parameter path
+        /// </summary>
+        /// <param name="path">path to the file to sort</param>
+        private static void SortFilesContents(string path)
+        {
+            var sorter = new Sorter();
+            sorter.SortFileContentsAlphabetically(path);
+        }
+    }
+}
---
+++ AlphabetSorter/Sorter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSorter
+{
+    /// <summary>
+    /// Class for sorting file contents alphabetically which is outputted to a second
+    /// file with "Sorted" appended to the name.
+    /// </summary>
+    public class Sorter
+    {
+        /// <summary>
+        /// Sorts the contents of a file alphaebtically into a second file with the same name
+        /// as the file the path param points to, with "Sorted" appended to the file name
+        /// </summary>
+        /// <param name="path">path to the file to sort.</param>
+        public void SortFileContentsAlphabetically(string path)
+        {
+            var content = File.ReadAllLines(path).ToList();
+            var sortedContent = content.OrderBy(element => element);
+            var sortedFileLocation = GetAbsolutePathToSortedFile(path);
+            WriteStringsToFile(sortedContent, sortedFileLocation);
+        }
+
+        /// <summary>
+        /// Write the elements of a collection of strings to a file, the absolute path to which
+        /// is specified as the second parameter.
+        /// </summary>
+        /// <param name="content">Collection of strings, each item a line to write as content to the new file</param>
+        /// <param name="filepath">Absolute path to the file to write to</param>
+        private void WriteStringsToFile(IEnumerable<string> content, string filepath)
+        {
+            File.WriteAllLines(filepath, content);
+        }
+
+        /// <summary>
+        /// Creates the absolute path to the file to contain the sorted content from the
+        /// path to the file to sort.
+        /// </summary>
+        /// <param name="path">path to the file to sort</param>
+        /// <returns>string path to file to receive sorted content</returns>
+        private string GetAbsolutePathToSortedFile(string path)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            var location = Path.GetFullPath(path).Substring(0, path.LastIndexOf('\\'));
+            return location + "\\" + fileName + "Sorted.txt"; 
+        }
+        
+    }
+}
